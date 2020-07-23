43d4729f5715f1bb99a086c00c5a7931795c1ee3
fhs42724 <abitzan-mmtb2018@fh-salzburg.ac.at>
2020-07-23 12:52:46 +0200

    Layout Fixes

--- src/components/fancyinfopanel.js
+++ src/components/fancyinfopanel.js
@@ -6,13 +6,13 @@ import { Parallax, ParallaxProvider } from "react-scroll-parallax"
 const Infobox = props => {
   if (props.imgData !== undefined) {
     return (
-      <ParallaxProvider>
+
         <BackgroundImage Tag="section" fluid={props.imgData} className="bgImg">
-          <Parallax y={[-30, 20]} tagOuter="figure">
+
             <div className="textTextWindow">{props.children}</div>
-          </Parallax>
+
         </BackgroundImage>
-      </ParallaxProvider>
+
     )
   } else {
     return <div className="textTextWindow">{props.children}</div>
